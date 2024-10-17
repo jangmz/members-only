ceac44048ce6fe9a54c8ca6a177bbd3cf6227a86
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-17 11:40:18 +0200

    initial code

---
+++ server.js
@@ -0,0 +1,25 @@
+import pg from "pg";
+import {fileURLToPath} from "url";
+import path from "path";
+import dotenv from "dotenv";
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import homeRouter from "./routes/homeRoute.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+// setting public files and EJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+// routes
+app.use("/", homeRouter);
+
+// app running
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
