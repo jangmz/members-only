702949b6e4234e773d1b5fe0556e077545795c57
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-22 14:03:13 +0200

    LocalStrategy configuration: matching username, matching password

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,7 @@
-//import db from "../db/queries.js";
+import db from "../db/queries.js";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import bcrypt from "bcryptjs";
 
 // GET /log-in -> show form
 function loginGet(req, res) {
@@ -10,6 +13,25 @@ function loginPost(req, res) {
     console.log("Check log in");
     res.redirect("/");
 }
+
+// LocalStrategy setup
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        // find username which is trying to log in
+        const user = db.findUsername(username);
+        console.log(user);
+        if (!user) return done(null, false, { message: "Incorrect username!" });
+
+        // password comparison -> plain to hashed
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) return done(null, false, { message: "Incorrect password!" });
+
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
 
 export default {
     loginGet,
