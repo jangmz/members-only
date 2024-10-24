0ef4c71ceea478ffbc2e248591715ac2485409f1
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-24 14:33:58 +0200

    rearanged code

--- routes/logoutRouter.js
+++ routes/logoutRouter.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 const logoutRouter = Router();
 
-logoutRouter.get("/", (req, res) => {
-    req.logout();
+logoutRouter.get("/", (req, res, next) => {
+    req.logout(error => {
+        if (error) return next(error);
+    });
     res.redirect("/log-in");
 });
 
