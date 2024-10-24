70cd7d8fc007399b6b60c3510f7e89d40fc5d368
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-24 14:27:21 +0200

    login authentication and verification

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,28 +3,16 @@ import passport from "passport";
 import LocalStrategy from "passport-local";
 import bcrypt from "bcryptjs";
 
-// GET /log-in -> show form
-function loginGet(req, res) {
-    res.render("log-in");
-}
-
-// POST /log-in -> check log in in db
-function loginPost(req, res) {
-    console.log("Check log in");
-    res.redirect("/");
-}
-
-// LocalStrategy setup
+// passport configuration (strategy) -> LocalStrategy setup
 passport.use(new LocalStrategy(async (username, password, done) => {
     try {
         // find username which is trying to log in
-        const user = db.findUsername(username);
-        console.log(user);
+        const result = await db.findUsername(username);
+        const user = result[0];
         if (!user) return done(null, false, { message: "Incorrect username!" });
 
         // password comparison -> plain to hashed
         const match = await bcrypt.compare(password, user.password);
-
         if (!match) return done(null, false, { message: "Incorrect password!" });
 
         return done(null, user);
@@ -33,7 +21,29 @@ passport.use(new LocalStrategy(async (username, password, done) => {
     }
 }));
 
+// session and serialization, session creation
+// user available through req.user
+passport.serializeUser(async (user, done) => {
+    done(null, user.id);
+});
+
+// retrieving ID from stored session
+passport.deserializeUser(async (id, done) => {
+    try {
+        const result = await db.findUserById(id);
+        const user = result[0];
+        done(null, user); // user available in req.user
+    } catch (error) {
+        done(error);
+    } 
+})
+
+// GET /log-in -> display form
+function loginGet(req, res) {
+    res.render("log-in");
+}
+
 export default {
     loginGet,
-    loginPost,
+    //loginPost,
 };
