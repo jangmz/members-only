a34506cd44a628c6a94592358a4b46942acc6398
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-24 14:28:24 +0200

    passport initialization, logout route, TODO: user middleware

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 import session from "express-session";
+import passport from "passport";
 import homeRouter from "./routes/homeRoute.js";
 import loginRouter from "./routes/loginRouter.js";
 import signupRouter from "./routes/signupRouter.js";
+import logoutRouter from "./routes/logoutRouter.js";
 
 const app = express();
 dotenv.config();
@@ -21,12 +23,16 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// session config
+// session config (passport)
 app.use(session({
-    secret: "clubhouse",
+    secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
 }));
+app.use(passport.initialize());
 app.use(passport.session());
 //app.use(express.urlencoded({extended: false}));
 
@@ -34,6 +40,12 @@ app.use(passport.session());
 app.use("/", homeRouter);
 app.use("/log-in", loginRouter);
 app.use("/sign-up", signupRouter);
+app.use("/log-out", logoutRouter);
+
+// TODO: fix user authentication middleware
+app.use((req, res, next) => {
+    req.isAuthenticated() ? res.locals.user = req.user : res.locals.user = null;
+});
 
 // app running
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
